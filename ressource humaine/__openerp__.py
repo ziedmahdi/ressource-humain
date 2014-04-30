0c109c5084fc1c8a4c9c4305aebc9f1d07acc697
zied <ziedmahdi@gmail.com>
2014-04-30 17:02:24 +0100

    Ajout des fichiers suivants: .projet #pour eclipse .pydevproject #pour eclipe org.eclipse.core.resources.prefs #pour eclipse __init__.py __openerp__.py

---
+++ ressource humaine/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "",
+    "version": "1.0",
+    "depends": [""],
+    "author": "groupe",
+    "description": """""",
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
