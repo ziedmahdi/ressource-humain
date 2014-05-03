b7df07c74f311d7eecd9a7e91cd812349e347e82
zied <ziedmahdi@gmail.com>
2014-05-03 23:36:27 +0100

    Ajout des champs: *personel_phone *personel_email *ssn *address *zip *city *country *job

--- ressource humaine/inherit_hr.py
+++ ressource humaine/inherit_hr.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
-import openerp
+from openerp import addons
 from openerp import netsvc, tools, pooler
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
 
 class inherit_hr(osv.osv):
     
@@ -12,6 +12,14 @@ class inherit_hr(osv.osv):
     
     _columns = {
         'first_name':fields.char('First Name', size=64, required=True, readonly=False),
+        'personel_phone': fields.char('Personel Phone', size=32, readonly=False),
+        'personel_email': fields.char('Personel Email', size=240),
+        'ssn': fields.char('Security Social Number', size=32),
+        'address': fields.char('Address', size=240),
+        'zip': fields.char('Zip', size=32),
+        'city': fields.char('City', size=64),
+        'country': fields.char('Country', size=64),
+        'job': fields.char('Job', size=64)
         }
     
 inherit_hr() 
