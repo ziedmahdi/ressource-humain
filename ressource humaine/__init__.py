d096f45a713762c35ae143fbf8d5f5e712deff31
zineib <zinoubatr.1991@gmail.com>
2014-05-04 01:03:47 +0100

    le module centre de cout j'ai crée une nouvelle table, et j'ai pas encore fait la fonction calcul taux occupation

--- ressource humaine/__init__.py
+++ ressource humaine/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 import inherit_hr
+import cost_center
---
+++ ressource humaine/cost_center.py
@@ -0,0 +1,40 @@
+ #-*- coding: utf-8 -*-
+from openerp import addons
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp import tools
+
+class cost_center(osv.osv):
+    _name="cost.center"
+    _description=" centre de cout"
+    _columns = {
+              
+                'date_entry': fields.date('Date of Entry'), 
+                'date_release': fields.date('Realease Date'),
+                
+                #la fonction de l'employé je pense qu'il faut relier a HR.JOB
+                'function':fields.char('Function', size=64, required=True),
+                
+                #Un employé peut travailler en même temps dans 1 à n centres de coûts.
+                'employee_id': fields.many2one('hr.employee', "Employee", required=True),
+                'department_id': fields.related('employee_id','department_id', type='many2one', relation='hr.department', string="Department", readonly=True),
+                'number_of_hours_worked': fields.integer('Number of hours worked'),
+                'Departement_head':fields.char('Departement Head', size=64, required=True),
+
+            
+                    }
+    
+    def _check_dates(self, cr, uid, ids, context=None):
+        for date in self.read(cr, uid, ids, ['date_entry', 'date_release'], context=context):
+             if date['date_release'] and date['date_entry'] and date['date_entry'] > contract['date_release']:
+                 return False
+        return True 
+
+      
+
+    _constraints = [
+        (_check_dates, 'Error! Cost Center, date entry must be less than contract date release.', ['date_entry', 'date_release'])
+    ]    
+cost_center()
+    
+    
