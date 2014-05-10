ab2d1be9c320cc3e1a6a95b21bff12589db0bde7
zied <ziedmahdi@gmail.com>
2014-05-10 12:17:37 +0100

    création de la fonction qui controle la saisie des dates

--- ressource humaine/inherit_hr_contract.py
+++ ressource humaine/inherit_hr_contract.py
@@ -3,7 +3,7 @@ from openerp import addons
 from openerp import netsvc, tools, pooler
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
-import time
+import datetime
 
 class inherit_hr_contract(osv.osv):
     
@@ -11,11 +11,31 @@ class inherit_hr_contract(osv.osv):
     
     _inherit = 'hr.contract'
     
+    #function to make sure that the user enter valid start and end dates
+    def _check_dates(self, cr,uid,ids,context=None):
+        contracts = self.browse(cr,uid,ids,context=context)
+        for contract in contracts:
+            date_start = contract.date_start
+            if not date_start:
+                return False            
+            else:
+                date_start = datetime.strptime(date_start,"%d/%m/%Y")
+            date_end = contract.date_end
+            if not date_end:
+                return False
+            else:
+                date_end = datetime.strptime(date_end,"%d/%m/%Y") 
+            
+            if date_end > date_start:
+                return True
+            else:
+                return False
+            
+    
     _columns = {
                 'salary': fields.integer('Salaire',required=True),
                 'salary_type_id':fields.many2one('salary.type', 'Type de salaire', required=True), 
-                'date_start': fields.date('Date début',required=True), 
-                'date_end': fields.date('Date fin',required=True),
+                
                 
                 'work_time_type':fields.selection([
                      ('complet','Complet'),
@@ -25,6 +45,8 @@ class inherit_hr_contract(osv.osv):
                 'occupation_rate': fields.integer('Occupation rate'),
                 'taxed_at_source':fields.boolean('Tax at source' ), 
                 }
+    
+    _constraints = [(_check_dates, 'The date ...............', ['date_start','date_end'])] #to change
     
 
     
