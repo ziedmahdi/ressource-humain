62fadd8c6e8dd6382c1b31621113b3bbfc4f16d4
dragadan <seif.hammami20@gmail.com>
2014-05-13 00:37:02 +0100

    implémentation des fonctions qui controle la saisie sur le champ 'Salary'

--- ressource humaine/inherit_hr_contract.py
+++ ressource humaine/inherit_hr_contract.py
@@ -3,6 +3,7 @@ from openerp import addons
 from openerp import netsvc, tools, pooler
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
+import re
 import time
 
 class inherit_hr_contract(osv.osv):
@@ -11,8 +12,39 @@ class inherit_hr_contract(osv.osv):
     
     _inherit = 'hr.contract'  
     
+    #fonction qui vérifie la saisie du salaire lors d'un click (Onchange)
+    def onchange_salary(self,cr,uid,ids,salary):
+        if str(salary).isdigit():
+            return True
+        else:    
+            raise osv.except_osv(_('Invalid salary'), _('Please enter a valid salary'))
+           
+    #fonction qui vérifie la saisie du salaire après un click sur (Enregistrer)       
+    def _check_salary(self, cr,uid,ids,context=None):
+        contracts = self.browse(cr,uid,ids,context=context)
+        for contract in contracts:
+            salaire = contract.salary
+            if not salaire:
+                return False
+            else:
+                if salaire.isdigit():
+                    return True
+                
+                
+     #reste à implementer vu la relation avec le centre de cout              
+    '''def _check_occupation_rate(self, cr,uid,ids,context=None):
+        contracts = self.browse(cr,uid,ids,context=context)
+        for contract in contracts:
+            ocr = contract.occupation_rate
+            if not ocr:
+                return False
+            else: 
+                if 
+                else:
+                        return False  '''             
+    
     _columns = { 
-                'salary': fields.float('Salary',required=True),
+                'salary': fields.char('Salary'),
                 'salary_type_id':fields.many2one('salary.type', 'Salary type', required=True),                 
                 
                 'work_time_type':fields.selection([
@@ -20,11 +52,13 @@ class inherit_hr_contract(osv.osv):
                      ('partial','Partial'),
                      ],'Working time type', select=True),
                 
-                'occupation_rate': fields.integer('Occupation rate',required=True),
-                'taxed_at_source':fields.boolean('Taxed at source', required=True),
+                'occupation_rate': fields.integer('Occupation rate'),
+                'taxed_at_source':fields.boolean('Taxed at source'),
                 'cost_center_ids':fields.many2many('cost.center', 'contract_cost_center_rel', 'hr_contract_id', 'cost_center_id', 'Cost centers'),  
+    
                 }
     
+    _constraints = [(_check_salary, 'Invalid salary. Please enter a valid salary', ['salary'])]
 
     
 inherit_hr_contract()  
