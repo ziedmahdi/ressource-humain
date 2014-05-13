c29d25b9d47a6b27f9c193af341e251849476372
ziedmahdi <ziedmahdi@gmail.com>
2014-05-14 00:53:21 +0100

    Ajout du champ 'wage'. C'est un champ obligatoire qui ne peut pas être null. Je l'ai ajouté avec une valeur par défaut. (solution n'est pas trés convaicante mais ça marche)
    @Seif pas besoin de vérifier qu'un champs de type float ne reçoit que
    des entiers

--- ressource humaine/inherit_hr_contract.py
+++ ressource humaine/inherit_hr_contract.py
@@ -3,14 +3,13 @@ from openerp import addons
 from openerp import netsvc, tools, pooler
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
-import re
-import time
+
 
 class inherit_hr_contract(osv.osv):
     
     _name ='hr.contract' 
     
-    _inherit = 'hr.contract'  
+    _inherit = 'hr.contract'
     
     #fonction qui vérifie la saisie du salaire lors d'un click (Onchange)
     def onchange_salary(self,cr,uid,ids,salary):
@@ -18,33 +17,10 @@ class inherit_hr_contract(osv.osv):
             return True
         else:    
             raise osv.except_osv(_('Invalid salary'), _('Please enter a valid salary'))
-           
-    #fonction qui vérifie la saisie du salaire après un click sur (Enregistrer)       
-    def _check_salary(self, cr,uid,ids,context=None):
-        contracts = self.browse(cr,uid,ids,context=context)
-        for contract in contracts:
-            salaire = contract.salary
-            if not salaire:
-                return False
-            else:
-                if salaire.isdigit():
-                    return True
-                
-                
-     #reste à implementer vu la relation avec le centre de cout              
-    '''def _check_occupation_rate(self, cr,uid,ids,context=None):
-        contracts = self.browse(cr,uid,ids,context=context)
-        for contract in contracts:
-            ocr = contract.occupation_rate
-            if not ocr:
-                return False
-            else: 
-                if 
-                else:
-                        return False  '''             
+
     
     _columns = { 
-                'salary': fields.char('Salary'),
+                'salary': fields.float('Salary',required=True),
                 'salary_type_id':fields.many2one('salary.type', 'Salary type', required=True),                 
                 
                 'work_time_type':fields.selection([
@@ -52,13 +28,17 @@ class inherit_hr_contract(osv.osv):
                      ('partial','Partial'),
                      ],'Working time type', select=True),
                 
-                'occupation_rate': fields.integer('Occupation rate'),
-                'taxed_at_source':fields.boolean('Taxed at source'),
-                'cost_center_ids':fields.many2many('cost.center', 'contract_cost_center_rel', 'hr_contract_id', 'cost_center_id', 'Cost centers'),  
-    
+                'occupation_rate': fields.integer('Occupation rate',required=True),
+                'taxed_at_source':fields.boolean('Taxed at source', required=True),
+                'cost_center_ids':fields.many2many('cost.center', 'contract_cost_center_rel', 'hr_contract_id', 'cost_center_id', 'Cost centers'),
+                #juste pour eviter le problème de contraint not null sur wage 
+                'wage': fields.float('Wage', digits=(16,2), required=True, help="Basic Salary of the employee"),  
                 }
     
-    _constraints = [(_check_salary, 'Invalid salary. Please enter a valid salary', ['salary'])]
+    _defaults = {
+                 'wage' : 0
+        }
+    
 
     
 inherit_hr_contract()  
