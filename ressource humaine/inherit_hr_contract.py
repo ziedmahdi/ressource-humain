e28014e26a9a135bd08d3eaf63afc0a6c54a0f44
ziedmahdi <ziedmahdi@gmail.com>
2014-05-14 03:28:40 +0100

    Ajout de la fonction qui calcule le taux d'occupation d'un contrat(inherit_hr_contract)

--- ressource humaine/inherit_hr_contract.py
+++ ressource humaine/inherit_hr_contract.py
@@ -18,6 +18,26 @@ class inherit_hr_contract(osv.osv):
         else:    
             raise osv.except_osv(_('Invalid salary'), _('Please enter a valid salary'))
 
+    #function which calculat the occupation rate for a contract
+    def _get_contract_occupation_rate(self, cr, uid, ids, field_name, args, context=None):
+        result = {}
+        for i in ids:
+            
+            sql_req =""" SELECT occupation_rate AS occupation_rate
+                         FROM cost_center,contract_cost_center_rel
+                         WHERE (contract_cost_center_rel.hr_contract_id=%d)
+                         AND (contract_cost_center_rel.cost_center_id=cost_center.id)
+                         
+                         """ % (i,)
+#                      AND cost_center.active = TRUE
+            cr.execute(sql_req)
+            sql_res = cr.dictfetchall()
+            result[i]=0
+            if sql_res:                
+                for lign in sql_res:
+                    result[i] += lign['occupation_rate']                 
+        return result           
+        
     
     _columns = { 
                 'salary': fields.float('Salary',required=True),
@@ -28,7 +48,7 @@ class inherit_hr_contract(osv.osv):
                      ('partial','Partial'),
                      ],'Working time type', select=True),
                 
-                'occupation_rate': fields.integer('Occupation rate',required=True),
+                'occupation_rate': fields.function(_get_contract_occupation_rate, string='Occupation rate', type='integer', help='Occupation rate calculated based on the cost center related to this contract',readonly=True),
                 'taxed_at_source':fields.boolean('Taxed at source', required=True),
                 'cost_center_ids':fields.many2many('cost.center', 'contract_cost_center_rel', 'hr_contract_id', 'cost_center_id', 'Cost centers'),
                 #juste pour eviter le problème de contraint not null sur wage 
