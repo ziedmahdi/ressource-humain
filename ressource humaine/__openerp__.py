3a45dee74b032564ce6571e4ecfd06486a4884b0
dragadan <seif.hammami20@gmail.com>
2014-06-18 20:47:42 +0100

    Changement du name ,description , author ...

--- ressource humaine/__openerp__.py
+++ ressource humaine/__openerp__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Employee Details",
+    "name": "Human Resource Management",
     "version": "1.0",
     'category': 'Human Resources',
     "depends": ["base","hr","hr_contract","report_webkit"],
-    "author": "groupe pcd",
-    "description": """test zied""",
+    "author": "Feki Zied,Hammami Seifeddine,Trabelsi Zeineb",
+    "description": """The object of the present application is to centralize information and to homogenize
+procedures for managing human resources.
+This application will focus on three complementary aspects:
+Monitoring and managing employees,
+Monitoring and management of contracts and cost centers,and 
+Management of reports""",
     "init_xml": [],
     'update_xml': ["inherit_hr.xml",
 		   "cost_center.xml",
